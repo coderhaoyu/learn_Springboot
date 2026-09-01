b5647d593fe5e1491bb1d7e1c8a3d713d7902782
wuhaoyu <wuhaoyu@ajia.cn>
2026-09-01 16:14:39 +0800

    add SecurityConfig, update User.java

--- src/main/java/com/example/userdemo/entity/User.java
+++ src/main/java/com/example/userdemo/entity/User.java
@@ -9,6 +9,8 @@ public class User {
 
     private String email;
 
+    private String password;
+
     public User() {
 
     }
@@ -51,5 +53,13 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
